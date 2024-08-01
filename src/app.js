0ba9b0ca176d151b29b7ba7b5381e02ebe1aaeb6
Mohammadreza-24 <mohammadreza.bayat.2424@gmail.com>
2024-08-01 17:55:42 +0330

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
+function searchCity(city) {
+  let apiKey = "f48be877d847b465b9d3fc1a539f1bd7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+searchCity("Berlin");
