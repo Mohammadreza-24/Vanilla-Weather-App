a3fdb6e148d83e4992901796993c9b7026ccc077
Mohammadreza-24 <mohammadreza.bayat.2424@gmail.com>
2024-08-03 18:51:38 +0330

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,52 @@
+let dateTime = document.querySelector("#time");
+let currentDate = new Date();
+function formatDate(date) {
+  let day = date.getDate();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let month = date.getMonth();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let formattedMonth = months[month];
+  return `${formattedMonth} ${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let precipitationElement = document.querySelector("#precipitation");
+  let humidityElement = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind-speed");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  precipitationElement.innerHTML = response.data.clouds.all;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  dateTime.innerHTML = formatDate(currentDate);
 }
 
 function searchCity(city) {
